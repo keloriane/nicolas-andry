6fce925790e9ed5b859eb0e9da849b87297b909e
Kevin Flabat <kevin.flbt@gmail.com>
2024-05-02 15:10:01 +0200

    eslint fix and grid

--- sanity/lib/store.ts
+++ sanity/lib/store.ts
@@ -1,8 +1,5 @@
 import * as queryStore from "@sanity/react-loader";
 
-import { client } from "./client";
-import { token } from "./token";
 
-queryStore.setServerClient(client.withConfig({ token }));
 
 export const { loadQuery } = queryStore;
--- sanity/lib/token.ts
+++
@@ -1,9 +0,0 @@
-import "server-only";
-
-import { experimental_taintUniqueValue } from "react";
-
-export const token = process.env.SANITY_API_READ_TOKEN;
-
-if (!token) {
-  throw new Error("Missing SANITY_API_READ_TOKEN");
-}
